3d0c8564d736b6d6335612c7eb04fc5a62c52021
sinnryoku <mm291ie637nl.id458oa.nv.se@docomo.ne.jp>
2024-05-21 01:14:27 +0900

    hinapen display

--- src/pages/hinapen.tsx
+++ src/pages/hinapen.tsx
@@ -1,5 +1,6 @@
 import {NavLink} from "react-router-dom";
 import './hinapen.css';
+import hinapen from '../../public/hinapen.jpg'
 
 export const Hinapen = () => {
   const handleClick1 = () => {
@@ -9,7 +10,17 @@ export const Hinapen = () => {
     <div className="home">
       <NavLink to="/">ホームへ戻る</NavLink>
       <button onClick={handleClick1}></button>
-      <h1 className="title">ひなぺんの部屋の部屋</h1>
+      <div className="title">
+        <h1> Deepen </h1>
+        <h1>ひなぺんの部屋</h1>
+        <h1> テキストの翻訳 </h1>
+      </div>
+      <div className="option">
+        <h2 className="option1">雛ペンに会いに行く！</h2>
+        <h2 className="option2">図鑑を見る</h2>
+      </div>
+      <img src={hinapen} className="" alt="" />
+
     </div>
   );
 };
