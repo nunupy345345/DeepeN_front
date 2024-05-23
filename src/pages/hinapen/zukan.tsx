32ab6b74128ddb075d137a5cf63cc509322b5df2
sinnryoku <mm291ie637nl.id458oa.nv.se@docomo.ne.jp>
2024-05-23 18:00:15 +0900

    fix zukan

--- src/pages/hinapen/zukan.tsx
+++ src/pages/hinapen/zukan.tsx
@@ -1,14 +1,31 @@
 import {NavLink} from "react-router-dom";
 import './zukan.css';
-// import dict from '../../../public/image/images.tsx'
+import pen from '../../../public/image/pen.png';
+import koalapen from '../../../public/image/koalapen.png';
+import hideripen from '../../../public/image/hideri-pen.png';
+import peso from '../../../public/image/peso.png';
 
 
 export const Zukan = () => {
   return (
     <div>
-      <h1>図鑑</h1>
       <NavLink to="/hinapen">雛ペンのお部屋へ戻る</NavLink>
-      {/* <img src={dict["pen"]} className="image" /> */}
+      <div className="title">
+        <h1> Deepen </h1>
+        <h1>ひなぺんの部屋</h1>
+        <h1> テキストの翻訳 </h1>
+      </div>
+      <div className="zukan">
+        <h1 className="Zukan">図鑑</h1>
+        <div className="picture">
+          <img src={pen}/>
+          <img src={hideripen}/>
+          <img src={koalapen}/>
+          <img src={peso}/>
+        </div>
+
+      </div>
+      
       
     </div>
   );
