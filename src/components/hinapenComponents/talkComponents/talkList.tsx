a6e7b64192100b2764f13a0e503f00e8f5bdbaa9
sinnryoku <mm291ie637nl.id458oa.nv.se@docomo.ne.jp>
2024-06-06 21:23:49 +0900

    fix

--- src/components/hinapenComponents/talkComponents/talkList.tsx
+++ src/components/hinapenComponents/talkComponents/talkList.tsx
@@ -6,10 +6,10 @@ import hinapen from '../../../../public/images/hinapen.jpg'
 const array: readonly string[][] = [
     ["ぺン　ぺーぺ　ーぺぺン　ン　ーーンぺ　ぺぺぺン　ン　ーぺぺン　ーぺーン　ンンー　ぺーー　ーぺ　ンぺン　ぺぺぺ　ーンぺ　ーンーン　ぺーンン","最近、勉強捗ってる？",dict[0]],
     ['ぺ　ン　ぺぺンぺ　ン　ぺぺぺぺ　ぺンー　ぺーン　ぺンンー　ぺぺーぺ　ぺーー　ぺぺ　ーンーン　ぺーンン','ペンギンを見たことはある？',dict[1]],
-    ["2",'夏休みまであと少し　頑張ろう!',dict[2]],
+    ['ンンぺー　ンンンー　ーンンン　ーぺー　ぺンー　ーぺーぺ　ぺンぺン　ぺぺ　ぺぺーぺ　ーぺー　ぺンンー　ーぺぺぺ　ーーンぺ　ぺーンー　ン　ンぺぺン　ンぺンー　ンンー　ンーぺ','夏休みまであと少し　頑張ろう!',dict[2]],
     ["ーンンン　ぺぺぺ　ぺーぺぺ　ーーンー　ぺぺぺー　ーぺ　ーンー　ンぺぺー　ンーー　ーンー","やっぱりお金だよね",dict[3]],
-    ["4",'ちゃんと自炊してる？',dict[4]],
-    ["5",'皇帝ペンギンに会えた君はついてるね！',dict[5]],
+    ['ンぺ　ぺーーぺ　ン　ぺぺーぺ　ーぺンー　ーぺー　ぺーぺ　ーぺぺぺ　ーンぺ　ーンーン　ぺーンン','ちゃんと自炊してる？',dict[4]],
+    ['ぺンンー　ンンー　ーンぺ　ぺーぺ　ぺ　ン　ぺぺンぺ　ン　ンンぺ　ぺぺ　ーンぺー　ぺーン　ーぺぺン　ぺンー　ぺーー　ンンンー　ぺーぺ　ーンぺ　ーンーン　ーンー','皇帝ペンギンに会えた君はついてるね！',dict[5]],
     ['ぺぺ　ンンぺー　ぺーン　ぺーー　ーーンぺ　ーンンー　ぺぺーぺ　ーーンー　ぺぺーぺ　ンーーー　ンンぺ　ン　ーーンぺ　ンぺン　ンぺ　ンンンン　ンンンぺ　ぺーぺ　ンンンぺ　ンぺ　ぺぺぺぺ　ーンぺー　ンンンン　ぺンぺー　ーぺーぺ　ーぺー　ーぺ　ぺーンン','あなたは、1人と5人、どちらの命を選びますか？',dict[6]],
     ['ぺぺ　ぺーぺ　ぺぺぺぺ　ぺンンー　ンぺぺン　ーンンぺ　ぺぺぺー　ーぺーぺ　ーンぺー　ぺーー　ぺーン　ンぺぺー　ンンンぺ　ぺぺぺー　ぺンーン　ぺンぺー　ーぺーン　ンンー　ぺンぺ　ンンンぺ　ンぺぺー','愛を拒むお前はただの臆病者だ',dict[7]],
     ['ぺ　ン　ぺぺンぺ　ン　ぺンぺン　ぺぺ　ーンーン　ーぺ　ぺぺンぺ　ーーンー　ぺーぺ　ンンンー　ーぺ　ぺーー　ぺぺ　ぺーぺ　ーぺー　ーンーン　ぺンぺ　ンンンぺ　ぺぺーぺ　ンンンぺ　ぺぺぺン　ンンンー　ーーンー　ぺーンー　ーぺーぺ　ぺぺぺ　ーンぺ　ぺーぺ　ーンーン　ーーンぺ　ぺンー　ン　ンンぺー　ンーン　ンぺーー　ンンぺ　ぺーン　ーンぺー　ーンぺ　ぺーぺ　ーぺぺン　ーンぺ　ン　ンぺぺー　ンーー','ペンギンである限りいつかは愛するものとの別離が待っている　みんなそれに耐えて生きてんだよ！',dict[7]],
@@ -28,19 +28,20 @@ export const Response = () => {
     const [isShown,setIsShown] = useState(false);
     const [input, setInput] = useState('');
     const [output, setOutput] = useState('');
-    const [beforeNum, setBeforeNUM] = useState(0);
+    const [beforeNUM, setBeforeNUM] = useState(0);
     const [lang, setLang] = useState(0);    // 0→ペン語　1→日本語
     const [translate, setTranslate] = useState(false);
+    // const [a, setA] = useState(true);
 
-    const reset = () => {
+    const enter = () => {
         setIsShown(true)
         setOutput(input)
         setTranslate(false)
     };
-    const enter = () => {
+    const reset = () => {
         setIsShown(false)
         setBeforeNUM(number)
-        setTranslate
+        setTranslate(false)
     };
 
     const usingLang = () => {
@@ -50,6 +51,7 @@ export const Response = () => {
 
     const changeLang = (now:number) => {
         setTranslate(true);
+        setBeforeNUM(number)
         if (now == 0){ setLang(1) }
         else { setLang(0) }
     }
@@ -64,7 +66,6 @@ export const Response = () => {
                 <p className='here'>　　　　　　</p></>
             }
             else if (isShown==true && translate==true){
-                // setTranslate(false)
                 return <>
                 <p className={language[lang]}>{array[beforeNum][lang]}</p>
                 <img src={array[beforeNum][2]} alt="talkpen" className='talkpen'/>
@@ -87,9 +88,9 @@ export const Response = () => {
 
     return (
         <div className="talkSpace">
-            <input className="inputBox" type="text" value={input} onChange={(e) => setInput(e.target.value)} onClick={() => enter()} />
-            <button className="button" onClick={() => reset()}> 話す </button>
-            {showPhrase(output,beforeNum)}
+            <input className="inputBox" type="text" value={input} onChange={(e) => setInput(e.target.value)} onClick={() => reset()} />
+            <button className="button" onClick={() => enter()}> 話す </button>
+            {showPhrase(output,beforeNUM)}
             {usingLang()}
             <button className='changeLang' onClick={() => changeLang(lang)}> 変更 </button>
         </div>
